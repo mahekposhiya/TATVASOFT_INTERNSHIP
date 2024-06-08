6a14f43bdb89ac5dada78559064899d0ec0b99b0
mahek <mahekposhiya@gmail.com>
2024-06-08 22:42:11 +0530

    CIPlatfromWebAPI_PostgreSQL

---
+++ CIPlatfromWebAPI_PostgreSQL/Data_Access_Layer/Migrations/20240531082934_firstmigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserType",
+                table: "User",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserType",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
---
+++ CIPlatfromWebAPI_PostgreSQL/Data_Access_Layer/Migrations/20240531123028_secondmigration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data_Access_Layer.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MissionSkill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SkillName = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionSkill", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MissionTheme",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ThemeName = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MissionTheme", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Missions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MissionTitle = table.Column<string>(type: "text", nullable: false),
+                    MissionDescription = table.Column<string>(type: "text", nullable: false),
+                    MissionOrganisationName = table.Column<string>(type: "text", nullable: true),
+                    MissionOrganisationDetail = table.Column<string>(type: "text", nullable: true),
+                    CountryId = table.Column<int>(type: "integer", nullable: false),
+                    CityId = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "date", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "date", nullable: false),
+                    MissionType = table.Column<string>(type: "text", nullable: true),
+                    TotalSheets = table.Column<int>(type: "integer", nullable: true),
+                    RegistrationDeadLine = table.Column<DateTime>(type: "date", nullable: true),
+                    MissionThemeId = table.Column<string>(type: "text", nullable: false),
+                    MissionSkillId = table.Column<string>(type: "text", nullable: false),
+                    MissionImages = table.Column<string>(type: "text", nullable: true),
+                    MissionDocuments = table.Column<string>(type: "text", nullable: true),
+                    MissionAvilability = table.Column<string>(type: "text", nullable: true),
+                    MissionVideoUrl = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Missions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MissionSkill");
+
+            migrationBuilder.DropTable(
+                name: "MissionTheme");
+
+            migrationBuilder.DropTable(
+                name: "Missions");
+        }
+    }
+}
